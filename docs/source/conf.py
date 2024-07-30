90a3c1fc9228cf9f1c477b3afc316fae57ef9681
Clarke Hardy <cahardy@stanford.edu>
2024-07-29 19:46:54 -0700

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []
 html_context = {
     'analytics-id': 'G-KMG1S5KJZC', # Google Analytics tracking ID
     'display_github': True, # Integrate GitHub
