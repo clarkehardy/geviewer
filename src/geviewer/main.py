7f14e0420ee80c66c154cd64c16cb29e071b9246
Clarke Hardy <cahardy@stanford.edu>
2024-08-22 18:56:05 -0700

    added module to convert to gev off screen

--- src/geviewer/main.py
+++ src/geviewer/main.py
@@ -1,23 +1,13 @@
 import os
 import argparse
+from pathlib import Path
 from geviewer.gui import launch_app
-
-
-def check_files(files):
-    """Check if the files are valid.
-    """
-    for file in files:
-        if not os.path.exists(file):
-            print('Error: {} does not exist'.format(file))
-            return False
-        if not (file.endswith('.gev') or file.endswith('.heprep') or file.endswith('.wrl')):
-            print('Error: {} is not a valid file'.format(file))
-            print('Valid file types are .gev, .heprep, and .wrl')
-            return False
-    return True
+from geviewer.utils import check_files
 
 
 def main():
+    """The main command-line entry point for GeViewer.
+    """
     parser = argparse.ArgumentParser(
         description = 'GeViewer is a lightweight, Python-based visualization tool for Geant4.',
         epilog = 'For more information, visit https://geviewer.readthedocs.io/en/latest/'
