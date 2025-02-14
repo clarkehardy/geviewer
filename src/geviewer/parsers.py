d52a9f0fa6def839013b12dfdbe30deaca62dc52
Clarke Hardy <cahardy@stanford.edu>
2025-02-14 11:36:56 -0800

    formatting

--- src/geviewer/parsers.py
+++ src/geviewer/parsers.py
@@ -30,9 +30,11 @@ class Parser:
         :return: The initialized template.
         :rtype: dict
         """
-        return  {'name': name, 'id': str(uuid.uuid4())[-12:], 'shape': '', 'points': [], 'mesh_points': [],\
-                 'mesh_inds': [], 'colors': [], 'visible': True, 'scalars': [], 'is_dot': False, \
-                 'is_event': False, 'mesh': None, 'has_actor': False, 'children': []}
+        return  {'name': name, 'id': str(uuid.uuid4())[-12:], \
+                 'shape': '', 'points': [], 'mesh_points': [], \
+                 'mesh_inds': [], 'colors': [], 'visible': True, \
+                 'scalars': [], 'is_dot': False, 'is_event': False, \
+                 'mesh': None, 'has_actor': False, 'children': []}
     
     
     def combine_mesh_arrays(self, points, cells, colors):
